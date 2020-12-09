dd29fe1bca906a84e690d742442714b249a8191c
Kiril Zafirov <kiril.zafirov@it-labs.com>
2020-12-09 23:46:40 +0100

    new msg

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
- 
-    var i = 0;
+module.exports = (str) => {
+    return `💩${str}💩`;
+}
 // "test": "echo \"Error: no test specified\" && exit 1"
- 
- function get() {
-            return i;
-        };
-        exports.set = function (val) {
-            i = val;
-        };
-        exports.increment = function () {
-            return ++i;
-        };
